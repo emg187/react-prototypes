70dcc28e0b83d9c3c52674944ed9b37015123b63
Eric George <geric738@aol.com>
2019-03-26 18:03:33 -0700

    React Functional Components - Eric George

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,27 @@
+import React from "react";
+import Table from "./table";
 
+const students = [
+    {
+        student: "Peter Parker", 
+        course: "Intro to Photography", 
+        grade: 85
+    }, 
+    {
+        student: "Bruce Banner",
+        course: "Psychology", 
+        grade: 91
+    }, 
+    {
+        student: "Tony Stark", 
+        course: "Mechanical Engineering", 
+        grade: 100
+    }
+];
 
+export default function App(){
+    return <div className="container">
+                <h1>Student Grade Table</h1>
+                <Table data={students}/>
+            </div>
+}
